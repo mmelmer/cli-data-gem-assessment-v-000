ffc669838eb6cfd2e7be8fa8f7df8f401181285b
mmelmer <mikemelmer@gmail.com>
2016-08-06 12:00:06 -0400

    added correct selectors for date, venue, bands; working on loop to add info from additional pages

--- new_gem/lib/new_gem/CLI.rb
+++ new_gem/lib/new_gem/CLI.rb
@@ -3,13 +3,12 @@ class NewGem::CLI
 
 require 'open-uri'
 require 'nokogiri'
-require 'watir'
-require 'headless'
  
-
+ @@shows_by_date = []
+ @@shows_by_venue = []
 
   def call
-    puts "Would you like to view today's shows or search by venue?"
+    puts "Would you like to check out today's shows or search by venue?"
     input = gets.strip
     while !input.include?("today") && !input.include?("venue")
       puts "I didn't understand your response."
@@ -17,15 +16,41 @@ require 'headless'
     end
     if input.include?("today") #<---check that include? method works on strings
       date_listings
-    elsif input.include?("venue") 
+    elsif input.include?("venue")
       venue_listings
     end
   end
 
 
   def date_listings
-    puts "these are today's shows..."
-    home = Nokogiri::HTML(open("http://www.ohmyrockness.com/shows"))
+    i = 2
+    #while home.css(".ds-paging").text.strip.include?("Next Page")
+    puts "loading today's shows..."
+    puts "\n"
+    home = Nokogiri::HTML(open("http://nyc-shows.brooklynvegan.com/events/today"))
+    while home.css(".ds-paging").text.strip.include?("Next Page")
+      home.css(".ds-event-category-music").each do |x|
+        puts x.css(".ds-venue-name > a span").text + ":"
+        puts x.css(".ds-listing-event-title-text").text
+        puts x.css(".dtstart").text.strip 
+        puts "\n"
+        home = Nokogiri::HTML(open"http://nyc-shows.brooklynvegan.com/events/today?page=#{i}")
+      end 
+      i += 1
+    end
+    home.css(".ds-event-category-music").each do |x|
+        puts x.css(".ds-venue-name > a span").text + ":"
+        puts x.css(".ds-listing-event-title-text").text
+        puts x.css(".dtstart").text.strip 
+        puts "\n"
+      end
+
+      
+    #  home = Nokogiri::HTML(open"http://nyc-shows.brooklynvegan.com/events/today?page=#{i}")
+    #  i +=1
+    #home.css(".ds-event-category-music a span").each {|x| puts x.text}
+
+    #home.css(".ds-listing-details").css(".dtstart").each {|x| puts x.text.strip }
     binding.pry
   end
 
@@ -37,12 +62,12 @@ require 'headless'
   def venue_listings
     puts "Which venue would you like to check out?"
     input = gets.strip
-    headless = Headless.new
-    headless.start
-    info = Watir::Browser.new 
-    info.goto "http://www.ohmyrockness.com/venues/#{input}"
-    site = Nokogiri::HTML.parse(info.html)
-    headless.destroy
+    #headless = Headless.new
+    #headless.start
+    #info = Watir::Browser.new 
+    #info.goto "http://www.ohmyrockness.com/venues/#{input}"
+    #site = Nokogiri::HTML.parse(info.html)
+    #headless.destroy
     binding.pry
     puts "Here are the upcoming shows at #{input}:"
     # <show tag>.each do |show| puts "#{date tag}: #{each band tag}, #{time formatted within minimal digits}"
@@ -72,3 +97,28 @@ end
 
 # pry(#<NewGem::CLI>)> site.css("#upcomingShows").first.children.first.children[5].children[5].children.text.strip.split(", ")
 #
+
+# home.css(".ds-listing-details").first.css(".ds-venue-name").first
+
+
+
+
+
+
+# => venue name:   
+#   home.css(".ds-listing-details").first.css(".ds-venue-name > a span").text
+#
+# => band name:
+#   home.css(".ds-event-category-music a span")[1].text
+
+#  => show time:
+#   home.css(".ds-listing-details").first.css(".dtstart").text.strip 
+
+
+# http://nyc-shows.brooklynvegan.com/events/2016/08/05
+# http://nyc-shows.brooklynvegan.com/venues/barbes
+
+
+
+#home.css(".ds-event-category-music").first
+# 
--- new_gem/lib/show_listings.rb
+++ new_gem/lib/show_listings.rb
@@ -3,8 +3,6 @@ require_relative "new_gem/CLI"
 require 'pry'
 require 'open-uri'
 require 'nokogiri'
-require 'json'
-require 'headless'
 
 
 module NewGem
