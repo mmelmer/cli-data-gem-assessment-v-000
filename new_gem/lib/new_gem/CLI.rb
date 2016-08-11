b0afe9e65a037d7534046bea1f71996cb8ae6436
mmelmer <mikemelmer@gmail.com>
2016-08-10 21:41:05 -0400

    added launchy gem to launch browser from cli; added user interface options for show number, next page, end, etc.

--- new_gem/lib/new_gem/CLI.rb
+++ new_gem/lib/new_gem/CLI.rb
@@ -3,12 +3,12 @@ class NewGem::CLI
 
 require 'open-uri'
 require 'nokogiri'
- 
- @@shows_by_date = []
- @@shows_by_venue = []
+require 'launchy'
+
+  attr_accessor :i
 
   def call
-    puts "Would you like to check out today's shows or search by venue?"
+    puts "Hello! Would you like to check out today's shows or search by venue?"
     input = gets.strip
     while !input.include?("today") && !input.include?("venue")
       puts "I didn't understand your response."
@@ -23,41 +23,54 @@ require 'nokogiri'
 
 
   def date_listings
-    i = 2
+    @i = 2
     num = 1
     #while home.css(".ds-paging").text.strip.include?("Next Page")
+    puts "\n"
     puts "loading today's shows..."
     puts "\n"
     home = Nokogiri::HTML(open("http://nyc-shows.brooklynvegan.com/events/today"))
-    while home.css(".ds-paging").text.strip.include?("Next Page")
+    
+    if home.css(".ds-paging").text.strip.include?("Next Page")
       home.css(".ds-event-category-music").each_with_index do |x, index|
         puts "#{index+1}. " + x.css(".ds-venue-name > a span").text + ":"
         puts x.css(".ds-listing-event-title-text").text
         puts x.css(".dtstart").text.strip 
         puts "\n"
-        home = Nokogiri::HTML(open"http://nyc-shows.brooklynvegan.com/events/today?page=#{i}")
+        num +=1
       end 
-      i += 1
-    end
-    home.css(".ds-event-category-music").each do |x|
-        puts x.css(".ds-venue-name > a span").text + ":"
+      # home = Nokogiri::HTML(open"http://nyc-shows.brooklynvegan.com/events/today?page=#{i}")
+      puts "If you'd like to find out about one of these shows, enter its number. If you'd like to hear about more shows, type more. If you're finished, type done."
+      input_2 = gets.strip
+      if input_2.is_a?(Integer) && ((0 < input_2) && (input_2 < num))
+        return "I'll open that site for you..."
+        Launchy.open("tinymixtapes.com")
+      elsif input_2 == "more"
+        return "Here's the next page of shows..."
+        # i += 1
+      elsif input_2 == "done"
+        return "Goodbye. Check back tomorrow!"
+      else
+        return "I didn't understand your input. Please try again."
+      end
+    else
+      home.css(".ds-event-category-music").each_with_index do |x, index|
+        puts "#{index+1}. " + x.css(".ds-venue-name > a span").text + ":"
         puts x.css(".ds-listing-event-title-text").text
         puts x.css(".dtstart").text.strip 
         puts "\n"
+        puts "This is the end of today's listings. If you'd like to find out about one of these shows, type its number. If you're done, type done."
+        input_2 = gets.strip
+        return input_2
       end
-
+    end
+  end
+    
       
-    #  home = Nokogiri::HTML(open"http://nyc-shows.brooklynvegan.com/events/today?page=#{i}")
-    #  i +=1
     #home.css(".ds-event-category-music a span").each {|x| puts x.text}
 
     #home.css(".ds-listing-details").css(".dtstart").each {|x| puts x.text.strip }
-  end
 
-
-
-  def listings_by_date
-  end
 
   def venue_listings
     puts "Which venue would you like to check out?"
@@ -65,6 +78,7 @@ require 'nokogiri'
     input_format = input.gsub(/\s/, '-')
     cap = input.split(" ").each {|word| word.capitalize!}.join(" ")
     home = Nokogiri::HTML(open("http://nyc-shows.brooklynvegan.com/venues/#{input_format}"))
+    puts "\n"
     puts "These are the upcoming shows at #{cap}:"
     puts "\n"
     i = 1
@@ -78,13 +92,19 @@ require 'nokogiri'
     # date of show: home.css(".ds-events-group").first.css(".ds-event-date").text.strip
     # time of show: home.css(".ds-events-group").first.css(".ds-event-time").text.strip
     # bands playing: home.css(".ds-events-group").first.css(".ds-listing-event-title-text").text
-    puts "Would you like to find out more about any of these shows? If so, enter the show's number. If not, type no."
+    puts "Would you like to find out more about any of these shows? If so, enter the show's number. If you want to see more shows, type more. If you're done, type done."
     input_2 = gets.chomp
     input_int = input_2.to_i
-    if input_2.downcase == "no"
+    if input_2.downcase == "done"
+      puts "\n"
       puts "Have a nice day - check back tomorrow!"
-    elsif input_2.is_a?(Integer) && ((0 < input_2) && (input_2 < i))
-      puts "Here's the page for that show."
+    elsif input_2.downcase == "more"
+      puts "Here are some more listings..."
+      # recursive?
+    elsif ((0 < input_int) && (input_int < i))
+      puts "Opening the page for that show:"
+      Launchy.open("www.ohmyrockness.com")
+      # actual url: home.css('ds-events-group')[input_int].css('a href')  <-- or something like that
     elsif input_int > 0
       puts "Please enter a number between 1 and #{i-1}"
     else
@@ -94,6 +114,7 @@ require 'nokogiri'
     # <show tag>.each do |show| puts "#{date tag}: #{each band tag}, #{time formatted within minimal digits}"
   end  
 
+end
  # def venue_show_selection
  #   input = ""
  #   yes = ["yes", "Yes", "YES", "Y", "y"]
@@ -112,7 +133,6 @@ require 'nokogiri'
  #   end
  # end
 
-end
 
 
 
