4224e6a61f406013fdd580118ebb4d36b8d8599e
mmelmer <mikemelmer@gmail.com>
2016-10-04 19:02:27 -0400

    corrected environment to update classes

--- show_listings/config/environment.rb
+++ show_listings/config/environment.rb
@@ -5,6 +5,5 @@ require 'launchy'
 
 require_relative "../lib/show_listings/version"
 require_relative "../lib/show_listings/CLI"
-require_relative "../lib/show_listings/listing"
 require_relative "../lib/show_listings/scraper"
 require_relative "../lib/show_listings/event"
